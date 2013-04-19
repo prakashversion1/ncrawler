1fa9b2343116e49c52b91021b674e13b75abdcab
rho <rho.rhoit@gmail.com>
2013-04-19 16:55:28 +0545

    New Header

--- aljazeera/plain.sh
+++ aljazeera/plain.sh
@@ -10,7 +10,7 @@ cd final
 function extract_meta {
     sed -n '/meta.*"\(title\|author\)/p; /_CreateDate.*value/p; s/\(\r\|\t\)//g' $i >/tmp/metatext
     sed -e 's/\/>/&\n/g' -f "$WD/../ehtml.pattern"  -f "$WD/../alien.pattern" -i /tmp/metatext
-        
+
     title=$(sed -n 's/.*"title".*="\(.*\)".*/\1/p' /tmp/metatext)
     author=$(sed -n 's/.*"author".*="\(.*\)".*/\1/p' /tmp/metatext)
     date="$sub/$(sed -n 's/.*_CreateDate.*=\"\(.*\) [[:alpha:]]\{3\} [0-9]\{4\}.*/\1/p' /tmp/metatext)"
@@ -18,16 +18,16 @@ function extract_meta {
 
 for i in ../downloads/*/*/*; do
     sub=`echo $i | sed 's/.*\([0-9]\{4\}\/[0-9]\{2\}\).*/\1/g'`
-    
+
     name="`echo $i | sed -n 's/.*\/\(.*\)\..*/\1/p'`"
     mkdir -p $sub
-    
-    #meta-data    
+
+    #meta-data
     extract_meta
     if [ "$title" == "" ] || [ "$author" == "" ] || [ "$date" == "" ]; then continue; echo fail; fi
     echo $i
-    echo -e "$title\n$author\n$date\nhttp://www.aljazeera.com/indepth/opinion/$sub/$name.html\n" > $sub/$name.txt
-    sed -n -f ../plain.pattern $i | sed -f "$WD/../ehtml.pattern" >> $sub/$name.txt    
+    echo -e "# TITLE@$title\n# AUTHOR@$author\n# DATE@$date\n# URL@http://www.aljazeera.com/indepth/opinion/$sub/$name.html\n" > $sub/$name.txt
+    sed -n -f ../plain.pattern $i | sed -f "$WD/../ehtml.pattern" >> $sub/$name.txt
     echo -e "$title\t$author\t$date\t$name" >> "$NAME.csv"
 done
 
